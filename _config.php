14c7efee7b79b77935f22e869dd89fd875bf15b2
Simon Erkelens <simonerkelens@silverstripe.com>
2016-11-13 13:34:53 +1300

    Just echo it into the environment file

--- _config.php
+++ _config.php
@@ -2,9 +2,9 @@
 
 Authenticator::register_authenticator('Firesphere\\YubiAuth\\YubikeyAuthenticator');
 
-if(!SapphireTest::is_running_test() && !defined('YUBIAUTH_CLIENTID')) {
+if(!defined('YUBIAUTH_CLIENTID')) {
     throw new LogicException('YUBIAUTH_CLIENTID Must be enabled to use YubiAuth');
 }
-if(!SapphireTest::is_running_test() && !defined('YUBIAUTH_APIKEY')) {
+if(!defined('YUBIAUTH_APIKEY')) {
     throw new LogicException('YUBIAUTH_APIKEY Must be enabled to use YubiAuth');
 }
