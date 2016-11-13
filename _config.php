6a59b77ed469507bf9fd493987e47c68fff0e3f7
Simon Erkelens <simonerkelens@silverstripe.com>
2016-11-13 13:28:14 +1300

    We don't need the yubi api for testing

--- _config.php
+++ _config.php
@@ -2,9 +2,9 @@
 
 Authenticator::register_authenticator('Firesphere\\YubiAuth\\YubikeyAuthenticator');
 
-if(!defined('YUBIAUTH_CLIENTID')) {
+if(!SapphireTest::is_running_test() && !defined('YUBIAUTH_CLIENTID')) {
     throw new LogicException('YUBIAUTH_CLIENTID Must be enabled to use YubiAuth');
 }
-if(!defined('YUBIAUTH_APIKEY')) {
+if(!SapphireTest::is_running_test() && !defined('YUBIAUTH_APIKEY')) {
     throw new LogicException('YUBIAUTH_APIKEY Must be enabled to use YubiAuth');
 }
